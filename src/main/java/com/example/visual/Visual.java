16f3088f21b4f7c22d2cd6dbf12f3fc09f90d287
Matheus Carvalho <pasormatheus@live.com>
2026-03-30 19:03:46 -0300

    Implementação do botão excluir

--- src/main/java/com/example/visual/Visual.java
+++ src/main/java/com/example/visual/Visual.java
@@ -47,6 +47,8 @@ public class Visual {
         email.setBounds(10,80,150,50);
         JLabel senha = new JLabel("Senha: ");
         senha.setBounds(10,108, 150, 50);
+        JLabel id = new JLabel("ID: ");
+        id.setBounds(10,160,150,50 );
 
         //campos de texto  para inserir os dados 
         
@@ -56,6 +58,8 @@ public class Visual {
         campoEmail.setBounds(60, 93,300, 20);
         JTextField campoSenha = new JTextField(50);
         campoSenha.setBounds(60,125,300,20);
+        JTextField campoId = new JTextField(50);
+        campoId.setBounds(60, 190, 300 , 20);
     
 
 
@@ -71,6 +75,9 @@ public class Visual {
         painel.add(campoNome);
         painel.add(campoSenha);
         painel.add(senha);
+        painel.add(botaoExcluir(usuario, campoId, janelaPrincipal));
+        painel.add(campoId);
+        painel.add(id);
         
 
         //o que esta na janela principal
@@ -84,8 +91,8 @@ public class Visual {
 
     public static JButton  botaoInserir(Usuario usuario, JTextField campoNome, JTextField campoEmail, JTextField campoSenha, JFrame janelaPrincipal){
         
-        JButton botaoInserir = new JButton("Inserir");
-        botaoInserir.setBounds(150,180,80,30);
+        JButton botaoInserir = new JButton("Cadastrar");
+        botaoInserir.setBounds(150,150,95,30);
         botaoInserir.addActionListener(e -> {
             usuario.setNome(campoNome.getText());
             usuario.setEmail(campoEmail.getText());
@@ -106,6 +113,31 @@ public class Visual {
         
         return botaoInserir;
     }
+
+    public static JButton botaoExcluir(Usuario usuario,JTextField campoId, JFrame janelaPrincipal){
+       JButton botaoExcluir = new JButton("Excluir Coluna");
+       botaoExcluir.setBounds(135, 220, 130, 30);
+      
+       botaoExcluir.addActionListener(e ->{ 
+             DaoUsuario daoUsuario = new DaoUsuario(conn);
+             int id = Integer.parseInt(campoId.getText());
+             usuario.setId(id);
+
+             try {
+                daoUsuario.excluir(id);
+            } catch (SQLException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+
+            JOptionPane.showMessageDialog(janelaPrincipal, "Usuário: " + usuario.getNome() + 
+                "Email: " + usuario.getEmail() + "ID: " + usuario.getId() + "Cadsatro excluido com sucesso!!");
+
+       });
+       return botaoExcluir;
+    }
+
+
 
 
     
